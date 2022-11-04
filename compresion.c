d5a03bbb02f28a0298dc55244a16acc90f8a40d7
pmayavi <pmayav@eafit.edu.co>
2022-11-04 04:12:49 -0500

    Update compresion.c

--- compresion.c
+++ compresion.c
@@ -120,7 +120,7 @@ struct node *find(struct node *node, char c)
 void print_nodes(struct node *node)
 {
     // if (node->char_element)
-    printf("[%c,%d,%d]\n", node->char_element, node->uses, node->right->char_element, node->left->char_element);
+    printf("[%c,%d,%c, %c]\n", node->char_element, node->uses, node->right->char_element, node->left->char_element);
     if (node->right)
         print_nodes(node->right);
     if (node->left)
