1be330acfed532ee2d5793bfe28e0492f44fdf7a
pmayavi <pmayav@eafit.edu.co>
2022-11-04 10:13:46 -0500

    Update compresion.c

--- compresion.c
+++ compresion.c
@@ -97,6 +97,8 @@ int compare(struct node *node)
             temp = temp->parent;
             if (node->uses > temp->right->uses)
                 ex = temp->right;
+            else if (node->uses > temp->left->uses)
+                ex = temp->left;
         } while (temp->parent);
         // printf("node:%d > temp:%d\n", node->uses, ex->right->uses);
         if (ex)
