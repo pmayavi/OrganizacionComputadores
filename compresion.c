e2bd8cd71179a20e2a9b629ce927af57942f968e
pmayavi <pmayav@eafit.edu.co>
2022-11-03 19:58:11 -0500

    test

--- compresion.c
+++ compresion.c
@@ -54,11 +54,11 @@ void insert(struct node *father, struct node *node) // Function to insert a new 
 int main()
 {
     printf("boing1");
-    char input[MAX + 1] = {0};
+    char input[MAX] = {0};
     char letters[107];
     size_t let = 0;
     int num[107];
-    fgets(input, MAX, stdin);
+    fgets(input, 2, stdin);
     printf("boing1.1");
     int new;
     struct node *root = new_node(0, 0, NULL);
