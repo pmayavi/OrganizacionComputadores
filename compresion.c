6b7e6fb45e47b09b844004bffe72950497cc5d6f
pmayavi <pmayav@eafit.edu.co>
2022-11-03 23:07:39 -0500

    Update compresion.c

--- compresion.c
+++ compresion.c
@@ -84,7 +84,7 @@ int compare(struct node *node)
 {
     struct node *temp = node->parent;
     struct node *ex = NULL;
-    printf("%d-%d\n", temp->uses, node->parent->right->uses);
+    printf("%d-%d\n", node->uses, temp->parent->right->uses);
     if (node->uses > temp->parent->right->uses)
     {
         do
