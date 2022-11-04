b6cad860bf04e1dc1989511ab357413f6b405764
pmayavi <pmayav@eafit.edu.co>
2022-11-04 13:44:40 -0500

    Update compresion.c

--- compresion.c
+++ compresion.c
@@ -146,7 +146,7 @@ int main()
             int ch = input[i];
             printf("%c", -159);
             printf("%c", -164);
-            if ((ch < 32 || ch > 127) && (ch < 160 || ch > 165))
+            if ((ch < 32 || ch > 127) && (ch > -159 || ch < -164))
             {
                 switch (ch)
                 {
