53a07317fdc2c3349376457fd6896d8ad92a554e
pmayavi <pmayav@eafit.edu.co>
2022-11-03 21:19:39 -0500

    Andres

--- compresion.c
+++ compresion.c
@@ -10,17 +10,16 @@ struct node
     int data_element;
     int uses;
     char char_element;
-    struct node *left, *right, *father;
+    struct node *left, *right, *parent;
 };
 
-struct node *new_node(int uses, char char_element, struct node *father)
+struct node *new_node(int uses, char char_element)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node)); // Allocating memory to the node
     temp->data_element = 1;
     temp->uses = uses;
     temp->char_element = char_element;
-    temp->left = temp->right = NULL;
-    temp->father = father;
+    temp->left = temp->right = temp->parent = NULL;
     return temp;
 }
 
@@ -35,26 +34,81 @@ void display(struct node *root) // A function for the inroder traversal of the b
 }
 
 void insert(struct node *, struct node *);
-void insert(struct node *father, struct node *node) // Function to insert a new node
+void insert(struct node *parent, struct node *node) // Function to insert a new node
 {
-    if (father->char_element != 0)
+    if (parent->char_element != 0)
         return;
-    if (!father->right)
+    if (!parent->right)
     {
-        father->left = new_node(node->uses, 0, father);
-        father->right = node;
+        parent->left = new_node(node->uses, 0, parent);
+        parent->left->parent = parent;
+        parent->right = node;
+        node->parent = parent;
     }
     else
     {
-        insert(father->left, node);
-        insert(father->right, node);
+        insert(parent->left, node);
+        insert(parent->right, node);
     }
+}
+
+void exchange(struct node *node, struct node *objective)
+{
+    struct node *temp = node->parent;
+    node->parent = objective->parent;
+    objective->parent = temp;
+    node->parent->right = node;
+    objective->parent->left = objective;
+}
+
+void ParentValue(struct node *parent)
+{
+    int Lchild = parent->left->uses;
+    int Rchild = parent->right->uses;
+    parent->uses = Lchild + Rchild;
+}
+
+int compare(struct node *node)
+{
+    int nodeValue = node->uses;
+    int ciclo = 1;
+    struct node *temp = NULL;
+    struct node *ex = NULL;
+    if (node->parent->left->uses == node->parent->right->uses)
+    {
+        // cuando son iguales no pasa nada
+    }
+    if (node->uses > node->parent->left)
+    {
+        while (ciclo == 1)
+        {
+            temp = compareParents(node, node->parent); // metodo de comparar con el hermano del padre
+            if (temp)
+            {
+                ex = temp;
+            }
+        }
+        if (ex)
+        {
+            exchange(node->parent, ex);
+        }
+    }
+}
+
+int compareParents(struct node *node, struct node *parent)
+{
+    if (node->uses > parent->parent->right)
+    {
+        return parent;
+    }
+    return NULL;
 }
 
 int main()
 {
     fgets(input, MAX, stdin);
     char letters[107];
+    struct node *nodes[107];
     size_t let = 0;
     int new;
     struct node *root = new_node(0, 0, NULL);
@@ -63,17 +117,18 @@ int main()
         new = 1;
         for (size_t j = 0; j < strlen(letters); j++)
         {
-            printf("%c-%c\n", input[i], letters[j]);
             if (input[i] == letters[j])
             {
                 new = 0;
+                // aqui es repetido
                 break;
             }
         }
-        if (new)
+        if (new) // Nuevo character, nuevo nodo
         {
             letters[let++] = input[i];
-            insert(root, new_node(1, input[i], root));
+            nodes[i] = new_node(1, input[i]);
+            insert(root, nodes[i]);
         }
     }
     display(root); // Function to display the binary tree elements
